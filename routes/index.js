8ab634d4ad945d14681368ccc9a69ad4b455e9f3
kirlos basta <kirlosbasta@gmail.com>
2024-09-03 02:35:40 +0300

    Add GET /files with pagination

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
-import { postUpload, getShow, getIndex } from '../controllers/FilesController';
+import { postUpload, getIndex } from '../controllers/FilesController';
 import authenticate from '../middleware/authenticate';
 
 const router = Router();
@@ -14,7 +14,7 @@ router.get('/connect', getConnect);
 router.get('/disconnect', authenticate, getDisconnect);
 router.get('/users/me', authenticate, getMe);
 router.post('/files', authenticate, postUpload);
-router.get('/files/:id', authenticate, getShow);
+// router.get('/files/:id', authenticate, getShow);
 router.get('/files', authenticate, getIndex);
 
 export default router;

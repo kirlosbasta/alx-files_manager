cb129bee08b31339fd0f683a15ad12bac5865fda
kirlos basta <kirlosbasta@gmail.com>
2024-09-05 19:02:25 +0300

    Add main.test.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import thumbnail from 'image-thumbnail';
-import fs from 'fs';
-
-(async () => {
-  const thumb = await thumbnail(data, { width: 250 });
-  fs.writeFileSync('image_250', thumb);
-})();
